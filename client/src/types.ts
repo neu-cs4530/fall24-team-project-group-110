4481c67bc2f6ae6d232395636e01fab506e13130
jelliott2021 <elliott.joh@northeastern.edu>
2024-11-07 13:18:54 -0500

    added new changes to client Notification

--- client/src/types.ts
+++ client/src/types.ts
@@ -162,11 +162,18 @@ export interface User {
   notifications: Notification[];
 }
 
+/**
+ * Interface representing a notification object.
+ * - _id - The unique identifier for the notification.
+ * - type - The type of notification.
+ * - text - The content of the notification.
+ * - targetId - The ID of the associated object for the notification (e.g., Question or Conversation).
+ * - dateTime - The date and time when the notification was created.
+ */
 export interface Notification {
   _id: string;
   type: string;
   text: string;
-  link: string;
-  read: boolean;
+  targetId: string;
   dateTime: Date;
 }
