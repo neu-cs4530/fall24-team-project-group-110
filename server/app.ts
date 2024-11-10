5838cdcd8cc91d151e6b5fe46dd48583021b735d
jelliott2021 <elliott.joh@northeastern.edu>
2024-11-10 14:49:35 -0500

    undo resolve issue

--- server/app.ts
+++ server/app.ts
@@ -19,7 +19,6 @@ import userController from './controller/user';
 import conversationController from './controller/conversation';
 import messageController from './controller/message';
 import authController from './controller/auth';
-import { checkConversationAccess } from './models/application';
 import notificationController from './controller/notification';
 
 dotenv.config();
@@ -120,8 +119,8 @@ app.use('/tag', tagController());
 app.use('/answer', answerController(socket));
 app.use('/comment', commentController(socket));
 app.use('/user', userController(socket));
-app.use('/conversation', conversationController(socket));
-app.use('/message', messageController(socket));
+app.use('/conversation', conversationController());
+app.use('/message', messageController());
 app.use('/notification', notificationController());
 
 // Export the app instance
