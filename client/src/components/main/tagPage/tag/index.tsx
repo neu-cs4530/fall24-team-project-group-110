08fa708c9e2466837aa0e8944fa4c55ce3d882e5
John Elliott <61568949+jelliott2021@users.noreply.github.com>
2024-11-21 17:28:04 -0500

    Reworked tag page UI to be cleaner (#36)
    * Reworked tag page UI to be cleaner
    
    * fixed prettier errors
    
    * removed unused import

--- client/src/components/main/tagPage/tag/index.tsx
+++ client/src/components/main/tagPage/tag/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import { TagData } from '../../../../types';
 import useTagSelected from '../../../../hooks/useTagSelected';
 
-const { Title, Text } = Typography;
+const { Title } = Typography;
 
 /**
  * Props for the Tag component.
@@ -34,15 +34,39 @@ const TagView = ({ t, clickTag }: TagProps) => {
       onClick={() => clickTag(t.name)}
       style={{
         textAlign: 'center',
-        borderRadius: '8px',
+        borderRadius: '0px',
         boxShadow: '0 2px 8px rgba(0, 0, 0, 0.1)',
+        height: '170px',
+        position: 'relative',
+        paddingTop: '0px',
       }}>
-      <Badge.Ribbon text={`${t.qcnt} Questions`} color='blue'>
-        <Title level={5} style={{ color: '#1890ff', marginBottom: '8px' }}>
-          {tag.name}
-        </Title>
-        <Text type='secondary'>{tag.description}</Text>
-      </Badge.Ribbon>
+      <Badge.Ribbon
+        text={`${t.qcnt} Questions`}
+        color='blue'
+        style={{ top: '0', right: '0', position: 'absolute' }}></Badge.Ribbon>
+      <Title
+        level={5}
+        style={{
+          color: '#1890ff',
+          marginBottom: '8px',
+          fontSize: '200%',
+          marginTop: '16px',
+          whiteSpace: 'nowrap',
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
+        }}>
+        {tag.name}
+      </Title>
+      <span
+        style={{
+          display: '-webkit-box',
+          WebkitBoxOrient: 'vertical',
+          WebkitLineClamp: 3,
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
+        }}>
+        {tag.description}
+      </span>
     </Card>
   );
 };
