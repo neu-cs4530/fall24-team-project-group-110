b4228fc0ec023b4cee50dfbb3d46091013900b73
jackh4 <jackhuang460@gmail.com>
2024-11-22 13:28:14 -0500

    front end users page

--- client/src/components/main/usersPage/header/index.tsx
+++ client/src/components/main/usersPage/header/index.tsx
@@ -1,28 +1,38 @@
-import React from 'react';
-import { Input, Button, Row, Col } from 'antd';
+import React, { ChangeEvent, KeyboardEvent } from 'react';
+import { Input, Row, Col, Typography } from 'antd';
+import { CiSearch } from 'react-icons/ci';
+import './index.css';
+
+const { Title } = Typography;
 
 interface UserHeaderProps {
   search: string;
-  setSearch: (value: string) => void;
-  onSearch: () => void;
+  onSearch: (e: ChangeEvent<HTMLInputElement>) => void;
+  onKeyDown: (e: KeyboardEvent<HTMLInputElement>) => void;
 }
 
-const UserHeader: React.FC<UserHeaderProps> = ({ search, setSearch, onSearch }) => (
-  <Row gutter={16} className='user-header'>
-    <Col flex='auto'>
-      <Input
-        placeholder='Search for users'
-        value={search}
-        onChange={e => setSearch(e.target.value)}
-        allowClear
-      />
-    </Col>
-    <Col>
-      <Button type='primary' onClick={onSearch}>
-        Search
-      </Button>
-    </Col>
-  </Row>
+const UserHeader: React.FC<UserHeaderProps> = ({ search, onSearch, onKeyDown }) => (
+  <div className='user-header'>
+    <Title level={3} className='page-title'>
+      Users
+    </Title>
+    <Row gutter={16}>
+      <Col flex='auto'>
+        <Input
+          className='search-input'
+          addonBefore={<CiSearch />}
+          placeholder='Search for users...'
+          value={search}
+          onChange={onSearch}
+          onKeyDown={onKeyDown}
+          allowClear
+        />
+      </Col>
+    </Row>
+    <Title level={5} className='subheading'>
+      Find users across the platform
+    </Title>
+  </div>
 );
 
 export default UserHeader;
--- client/src/components/main/usersPage/user/index.tsx
+++ client/src/components/main/usersPage/user/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Card, Avatar, Typography } from 'antd';
+import { FaCircleUser } from 'react-icons/fa6';
 import { User } from '../../../../types';
+import './index.css';
 
 const { Text } = Typography;
 
@@ -10,16 +12,20 @@ interface UserViewProps {
 }
 
 const UserView = ({ user, onClick }: UserViewProps) => (
-  <Card hoverable onClick={onClick} style={{ textAlign: 'center', borderRadius: '8px' }}>
-    <Avatar
-      size={64}
-      src={user.picture || 'https://via.placeholder.com/64'}
-      alt={`${user.username} profile`}
-      style={{ marginBottom: '8px' }}
-    />
-    <Text strong>{user.username}</Text>
-    <div>
-      <Text type='secondary'>{`${user.firstName} ${user.lastName}`}</Text>
+  <Card hoverable onClick={onClick} className='user-card-style'>
+    <div className='user-card-content'>
+      <Avatar
+        size={48}
+        shape='circle'
+        src={user.picture || <FaCircleUser size={'10x'} color='#1e1e2f' />}
+        alt={`${user.username} profile`}
+        className='user-avator-style'
+      />
+      <div className='user-card-first-last-name'>
+        <Text strong>{user.username}</Text>
+        <br />
+        <Text type='secondary'>{`${user.firstName} ${user.lastName}`}</Text>
+      </div>
     </div>
   </Card>
 );
